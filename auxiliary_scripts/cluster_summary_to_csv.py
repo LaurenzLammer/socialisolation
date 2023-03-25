c2409f407189495f819ec45bfe390f8fc39bc184
LaurenzLammer <77966920+LaurenzLammer@users.noreply.github.com>
2023-03-25 23:53:59 +0100

    Update cluster_summary_to_csv.py

--- auxiliary_scripts/cluster_summary_to_csv.py
+++ auxiliary_scripts/cluster_summary_to_csv.py
@@ -1,8 +1,7 @@
-import numpy as np
 import pandas as pd
 import glob
 import os
-os.chdir('/data/pt_life/ResearchProjects/LLammer/Results/whole_brain/')
+os.chdir('/data/pt_life/ResearchProjects/LLammer/si_update/Results_whole_brain/')
 file_list = glob.glob('*_clusters_*.txt')
 overview = []
 for file in file_list:
@@ -16,7 +15,7 @@ for file in file_list:
             commentlines.append(line)
         else:
             resultlines.append(line)
-    if len(resultlines) > 1:
+    if len(resultlines) >= 1:
         resultlines.insert(0, commentlines[-1])
 
         for n in range(len(resultlines)):
@@ -24,11 +23,7 @@ for file in file_list:
             resultlines[n] = ' '.join(resultlines[n].split())
             resultlines[n] = resultlines[n].replace("\n", "")
             resultlines[n] = resultlines[n].split(" ")
-
-        csv_filename = name + ".csv"
         header = resultlines.pop(0)
-        df = pd.DataFrame(resultlines)
-        df.to_csv(path_or_buf= csv_filename, index=False, header=header)
         for n in range(len(resultlines)):
             resultlines[n].insert(0,name)
             overview.append(resultlines[n])
